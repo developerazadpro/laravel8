824425b77cc343a02ebc834c57587c34dd99fb0d
developerazad <azad@tst.com>
2022-02-07 22:26:38 +0600

    branch two file edit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/branch-two', function () {
+    return view('branchTwo.branch_two');
+});
